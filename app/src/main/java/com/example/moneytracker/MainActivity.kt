4de3cbea8d69a0bf8d50b7f83cc654d7bce26284
hyeonlee717 <hyeonlee717@gmail.com>
2024-10-04 08:21:41 +0900

    .

--- app/src/main/java/com/example/moneytracker/MainActivity.kt
+++ app/src/main/java/com/example/moneytracker/MainActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)  // XML 레이아웃 사용
+        setContentView(R.layout.activity_main)  // XML  레이아웃 사용
     }
 }
